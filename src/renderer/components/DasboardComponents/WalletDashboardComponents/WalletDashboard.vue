80bd3a966e04778258bbb7ff79e7924ad4b9e48d
Farina Ashfaq <fari.ash16@gmail.com>
2019-10-03 15:42:07 +0500

    ADD SQL DB

--- src/renderer/components/DasboardComponents/WalletDashboardComponents/WalletDashboard.vue
+++ src/renderer/components/DasboardComponents/WalletDashboardComponents/WalletDashboard.vue
@@ -11,7 +11,9 @@
 
 <script>
     import {getAllAcounts} from './walletcommon';
+    var sqlite3 = require('sqlite3').verbose();
     import Accounts from './Accounts';
+    var path = require('path')
     import LatestTransaction from './LatestTransaction';
     export default {
         name: 'WalletDashboard',
@@ -26,10 +28,23 @@
             'latesttransaction': LatestTransaction,
         },
         created(){
-            console.log(this.$router.history.current.path, this.$router.history)
-            if(this.$router.history.current.path === "/accountdetails"){
-                this.accdetails = false;
-            }
+            var db = new sqlite3.Database(path.join(__dirname, 'mygreatdb.sqlite3db'), (err, result) => {console.log(err, result)});
+
+            db.serialize(function() {
+                db.run("CREATE TABLE lorem (info TEXT)");
+
+                var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
+                for (var i = 0; i < 10; i++) {
+                    stmt.run("Ipsum " + i);
+                }
+                stmt.finalize();
+
+                db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+                    console.log(row.id + ": " + row.info);
+                });
+            });
+
+            db.close();
         },
         methods: {
         }
